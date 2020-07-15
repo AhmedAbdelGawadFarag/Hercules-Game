e4b173a8e26f5759cd9e30b4c6c36c6e3d3d64b0
Ahmed AbdelGawad Farag <ahmed.abdelgawad.farag@gmail.com>
2020-07-15 11:57:51 +0200

    initial commit

---
+++ desktop/src/com/hercules/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hercules.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hercules.Hercules;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Hercules(), config);
+	}
+}
