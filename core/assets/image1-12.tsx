804a297681ee94466bf38f0c74c1d861f17f31ad
abanoubmilad12@gmail.com <abanoubmilad12@gmail.com>
2020-07-18 13:21:03 +0200

    bug fix 2.0

--- core/assets/image1-12.tsx
+++ core/assets/image1-12.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="image1-12" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="C:/Users/PC/Downloads/image1-12.png" width="512" height="512"/>
+ <image source="image1-12.png" width="512" height="512"/>
 </tileset>
